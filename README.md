5ae678f2687a76cf13857876a83434a3e5fcc97e
unknown <d.whitelotusresort@gmail.com>
2020-11-21 22:56:36 +0700

    check

--- README.md
+++
@@ -1,2 +0,0 @@
-# eProject_Pascal-imitation-jewelry
-Project_nhom_ NAT_NTT_NSP_NNN
--- fb.js
+++
@@ -1,6 +0,0 @@
-// ---------------------------------------------------------------------Js cho ngoi sao stars-------------------------------------------------------------------------------------------
-function calcRate(r) {
-    const f = ~~r,//Tương tự Math.floor(r)
-    id = 'star' + f + (r % f ? 'half' : '')
-    id && (document.getElementById(id).checked = !0)
-   }
