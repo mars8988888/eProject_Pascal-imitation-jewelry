7fc3dc69026691ec94d927cd22136bbf2d365184
unknown <d.whitelotusresort@gmail.com>
2020-11-27 20:54:43 +0700

    ok

--- JS/gl.js
+++ JS/gl.js
@@ -14,3 +14,24 @@ function openCity(evt, cityName) {
 }
 
 
+// function myFunction(e) {
+//   // document.getElementById("gallery").style.width = "500px";
+//   // var y = e.clientY;
+//   var Count, n,m,i;
+//   var x = document.getElementsByClassName("gallery") 
+//   Count = x.style.width
+  
+//   for (i =0; i < Count; i++){
+//     n++;
+//     m = e.currentTarget.id += "n";
+//     document.getElementById("m").style.width = "500px";
+//   }
+      
+
+//   // // var coor = "Coordinates: (" + x + "," + y + ")";
+//   // document.getElementById("demo").innerHTML = coor;
+// }
+
+// function clearCoor() {
+//   document.getElementById("m").style.width = "200px"
+// }
