ed98d36c267b6e873d59275f2f78298ff32b48ab
unknown <d.whitelotusresort@gmail.com>
2020-11-27 16:57:18 +0700

    ok

---
+++ JS/fb.js
@@ -0,0 +1,6 @@
+// ---------------------------------------------------------------------Js cho ngoi sao stars-------------------------------------------------------------------------------------------
+function calcRate(r) {
+    const f = ~~r,//Tương tự Math.floor(r)
+    id = 'star' + f + (r % f ? 'half' : '')
+    id && (document.getElementById(id).checked = !0)
+   }
